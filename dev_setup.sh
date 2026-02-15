29a2700fbd3c0550c05b34c33bdfee20c9c51813
chLi <565353780@qq.com>
2026-02-15 08:49:10 +0800

    create setup bash files

---
+++ dev_setup.sh
@@ -0,0 +1,16 @@
+cd ..
+git clone github@github.com:565353780/base-gs-trainer.git
+
+conda install -y conda-forge::cgal
+
+cd base-gs-trainer
+./dev_setup.sh
+
+cd ../geo-gs/submodules/diff-gaussian-rasterization
+python setup.py install
+
+cd ../warp-patch-ncc
+python setup.py install
+
+cd ../tetra_triangulation
+python setup.py install
---
+++ setup.sh
@@ -0,0 +1,16 @@
+cd ..
+git clone http://github.com/565353780/base-gs-trainer.git
+
+conda install -y conda-forge::cgal
+
+cd base-gs-trainer
+./setup.sh
+
+cd ../geo-gs/submodules/diff-gaussian-rasterization
+python setup.py install
+
+cd ../warp-patch-ncc
+python setup.py install
+
+cd ../tetra_triangulation
+python setup.py install
